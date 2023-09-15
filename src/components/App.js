59c6857d859eec9a8bcccee23677fb6e002a0766
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-09-15 23:58:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    https://course.acciojob.com/web-idle?question=2c2f6ba8-fc16-48de-902e-4d418e04f026
-  </div>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
   )
 }
 
