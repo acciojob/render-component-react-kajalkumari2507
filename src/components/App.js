ad0859fe3f48f54f66acab4c1d43d971413f3465
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-09-15 23:55:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    https://course.acciojob.com/web-idle?question=2c2f6ba8-fc16-48de-902e-4d418e04f026
+  </div>
   )
 }
 
